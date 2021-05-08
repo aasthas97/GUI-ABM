17c489f1303bfb12aa19ec1d50d708d64f9dd709
Aastha <34710946+aasthas97@users.noreply.github.com>
2021-05-08 16:30:03 +0530

    Add player icon to window
    Changed window color, title and icon, and size. Added player icon at (0,0).

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
 import pygame
 
-size = width, height = 600, 400
+# initialize game
+pygame.init()
 
+# initialize window
+size = width, height = 600, 400
 screen = pygame.display.set_mode(size)
+pygame.display.set_caption('Grid game')
+
+# icons
+icon = pygame.image.load("face.jpg")
+pygame.display.set_icon(icon)
+
+# player icon
+playerIcon = pygame.image.load('player.png')
+
+def addPlayer():
+    screen.blit(playerIcon, (0, 0))
+
+# Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # quit button pressed
+            running = False
+    
+    screen.fill((255, 255, 255))
+    addPlayer()
+    pygame.display.update()
+   
