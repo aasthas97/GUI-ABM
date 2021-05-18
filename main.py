b0be824577b4523a30c859f26937b5bec65f0ec2
Aastha <34710946+aasthas97@users.noreply.github.com>
2021-05-18 15:49:24 +0530

    Player visible, no movement

--- main.py
+++ main.py
@@ -1,43 +1,40 @@
 import pygame
 
-def drawGrid(grid_size, window_size):
-    blockSize = window_size//grid_size #Set the size of the grid block
-    for x in range(window_size):
-        for y in range(window_size):
-            rect = pygame.Rect(x*blockSize, y*blockSize, blockSize, blockSize)
-            pygame.draw.rect(screen, (0, 0, 0), rect, 1)
-
-
 # initialize game
 pygame.init()
-grid_size = 4
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, X, Y, scale = None):
+        pygame.sprite.Sprite.__init__(self)
+        self.X = X
+        self.Y = Y
+        self.playerIcon = pygame.image.load('player.png')
+        self.rect = self.playerIcon.get_rect()
+        self.rect.center = (playerX, playerY)
+
+    def draw(self):
+        screen.blit(self.playerIcon, self.rect)
+
+
 
 # initialize window
-size = width, height = 400, 400
+size = width, height = 700, 700
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Reach the hospital')
 icon = pygame.image.load("face.jpg")
 pygame.display.set_icon(icon)
 
 # initialize player
-playerIcon = pygame.image.load('player.png')
-playerX = 0
-playerY = 0
-def addPlayer(x, y):
-    screen.blit(playerIcon, (x, y))
+playerX = 16
+playerY = 32
+player = Player(playerX, playerY)
 
 # initialize goal
 goalIcon = pygame.image.load('goal.png')
-goalX = 400-66
-goalY = 400-66
+goalX = width-66
+goalY = height-66
 def addGoal(x, y):
     screen.blit(goalIcon, (x, y))
-
-# detect goal reach
-def reachedGoal(player_pos_x, player_pos_y):
-    if goalX - player_pos_x < 40 and goalY - player_pos_y < 40:
-        return True
-    return False
 
 # Game loop
 running = True
@@ -46,7 +43,8 @@ while running:
         if event.type == pygame.QUIT: # quit button pressed
             running = False
 
-        if event.type == pygame.KEYDOWN: # a key has been pressed
+        # keyboard presses
+        if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 playerX -= 50
             if event.key == pygame.K_RIGHT:
@@ -59,24 +57,12 @@ while running:
     screen.fill((255, 255, 255))
 
     # Window boundaries
-    playerX = 0 if playerX < 0 else playerX
-    playerX = width-64 if playerX > width-64 else playerX # subtracting 64 because the player icon occupies 64 pixels
+    playerX = 16 if playerX < 16 else playerX
+    playerX = width-16 if playerX > width-16 else playerX # subtracting 64 because the player icon occupies 64 pixels
 
-    playerY = 0 if playerY < 0 else playerY
-    playerY = height-64 if playerY > height-64 else playerY
+    playerY = 32 if playerY < 32 else playerY
+    playerY = height-32 if playerY > height-32 else playerY
 
-    # check if player has reached goal
-    if reachedGoal(playerX, playerY):
-        over_font = pygame.font.Font('freesansbold.ttf', 28)
-        game_over_text = over_font.render('Game over!', True, (0, 0, 0))
-        screen.blit(game_over_text, (120, 150))
-        ### Uncomment to exit the window after game ends
-        # print('Game over.')
-        # break
-
-    # render
-    ### Uncomment the following to add grid
-    # drawGrid(grid_size, height)
-    addPlayer(playerX, playerY)
+    player.draw()
     addGoal(goalX, goalY)
     pygame.display.update()
