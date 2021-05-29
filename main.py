faabf90ebd888e7c77a1c956d7109154ca3d46ed
Aastha <34710946+aasthas97@users.noreply.github.com>
2021-05-29 12:01:50 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import pygame
 import numpy as np
 from pygame import draw
-import button
+from pygame_widgets import Slider
+# import ipywidgets as widgets
 
-"""Features: Start menu present, grid background absent"""
-
-# initialize game
-pygame.init()
-
+# CLASSES
 class Player(pygame.sprite.Sprite):
     def __init__(self, X, Y, scale = None, speed = 10):
         pygame.sprite.Sprite.__init__(self)
@@ -17,7 +14,7 @@ class Player(pygame.sprite.Sprite):
         self.flip = False
         self.playerIcon = pygame.image.load('./images/player.png')
         self.rect = self.playerIcon.get_rect()
-        self.rect.center = (playerX, playerY)
+        self.rect.center = (playerX+32, playerY+32)
         self.steal = 0
 
     def move(self, left, right, up, down):
@@ -38,156 +35,149 @@ class Player(pygame.sprite.Sprite):
 
         # update rectangle position
         self.rect.x += dx
-        self.rect.x = 0 if self.rect.x <0 else self.rect.x
-        self.rect.x = 650 if self.rect.x >650 else self.rect.x
-
         self.rect.y += dy
+        
+        # define x-axis boundaries
+        self.rect.x = 0 if self.rect.x <0 else self.rect.x
+        self.rect.x = 1040 if self.rect.x >1040 else self.rect.x
+
+        
+        # define y-axis boundaries
         self.rect.y = 0 if self.rect.y <0 else self.rect.y
-        self.rect.y = 538 if self.rect.y >538 else self.rect.y
+        self.rect.y = 436 if self.rect.y >436 else self.rect.y
+        
+    def collide(self, item):
+        collision_tolerance = 10
+        if abs(self.rect.bottom - item.rect.top) <= collision_tolerance: # top collision
+            self.rect.y -= self.speed
+        if abs(self.rect.top - item.rect.bottom) <= collision_tolerance: # bottom collision
+            self.rect.y += self.speed
+        if abs(self.rect.left - item.rect.right) <= collision_tolerance: # right collision
+            self.rect.x += self.speed
+        if abs(self.rect.right - item.rect.left) <= collision_tolerance: # left collision
+            self.rect.x -= self.speed
 
     def draw(self):
         screen.blit(pygame.transform.flip(self.playerIcon, self.flip, False), self.rect)
 
-# element class for graphics display
 class Graphics:
+    """Graphics to be displayed in game window"""
     def __init__(self,icon_path):
         self.icon = pygame.image.load(icon_path)
+        self.rect = self.icon.get_rect()
     
-    def draw(self, x, y):
-        element_rect = self.icon.get_rect()
-        element_rect.center = (x, y)
+    def draw(self, x, y, scale = 0):
+        if scale != 0:
+            self.icon = pygame.transform.scale(self.icon, scale)
+        self.rect.center = (x+32, y+32)
         screen.blit(self.icon, (x, y))
-        
 
-# class for on-screen text display        
+    def drawRect(self):
+        pygame.draw.rect(screen, (255, 0, 0), self.rect,  2)
+
 class Text:
+    """Text to be displayed in game window"""
     def __init__(self, text):
         self.text = text
         
-    def write(self, loc, color = (0, 0, 255)):
-        font = pygame.font.Font('freesansbold.ttf', 16)
-        screen.blit(font.render(self.text, True, color), loc)
-        
+    def write(self, loc, color = (0, 0, 255), font_size = 16):
+        font = pygame.font.Font('freesansbold.ttf', font_size)
+        screen.blit(font.render(self.text, True, color), loc)        
+
 
 # FUNCTIONS
-
 def addGameRect(screen, width, height):
     """Draw rectangle around the gaming part of the window"""
     pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(0, 0, width, height),  2)
     
-def reachedDanger(player_pos_x, player_pos_y):
-    """Check if player has reached danger"""
-    if dangerX - player_pos_x < 40 and dangerY - player_pos_y < 40:
-        return True
-    return False  
+def isCollide(item1, item2):
+    r1 = item1.rect
+    r2 = item2.rect
+    return r1.colliderect(r2)
 
-def reachedGoal(player_pos_x, player_pos_y):
-    if goalX - player_pos_x < 40 and goalY - player_pos_y < 40:
-        return True
-    return False
 
-# INITIALIZE VARIABLES
+# AGENT VALUES
 prob_risk_avoidance = np.random.random()
 prob_profit_seeking = np.random.random()
 learning_rate = np.random.random()
-print('Prob_risk_avoidance: %f\nprob_profit_seeking: %f\nlearning_rate: %f' % (prob_risk_avoidance, prob_profit_seeking, learning_rate))
 
+print(prob_risk_avoidance, prob_profit_seeking, learning_rate)
+probabilities = [Text('Risk avoidance: %.2f' % prob_risk_avoidance), Text('Profit-seeking: %.2f' % prob_profit_seeking), Text('Learning Rate: %.2f' % learning_rate)]
 
-risk_num= np.random.random() # risk value
-risk_string = 'Risk: ' + format(risk_num, '.2f')
-risk = Text(risk_string) # define class instance to be displayed on screen
+risk= np.random.random() # risk value
+cost = np.random.random()
+reward = np.random.random()
 
-# cost
-cost_num = np.random.random()
-cost_string = 'Cost: ' + format(cost_num, '.2f')
-cost = Text(cost_string)
+print('Risk: %f\nCost: %f\nReward: %f' % (risk, cost, reward))
+rrc = [Text('Risk: %.2f' % risk), Text('Cost: %.2f' % cost), Text('Reward: %.2f' % reward)]
 
-#reward
-reward_num = np.random.random()
-reward_string = 'Reward: ' + format(reward_num, '.2f')
-reward = Text(reward_string)
-
-print('%s\n%s\n%s' % (risk_string, cost_string, reward_string))
-
-R = prob_risk_avoidance * risk_num
-P = prob_profit_seeking * (reward_num-cost_num)
-net = P-R 
-rand = np.random.random()
-
-print('R: %f\nP: %f\nNet: %f\nrand: %f' % (R, P, net, rand))
-
-
-# START SCREEN
-start_img = pygame.image.load('./images/start.png')
-exit_img = pygame.image.load('./images/exit.png')
-start_button = button.Button(270, 200, start_img, 0.5)
-exit_button = button.Button(270, 500, exit_img, 0.5)
-
-# CREATE PLAYER
+# PLAYER
 playerX = 0
-playerY = 35
+playerY = 0
 player = Player(playerX, playerY)
 move_left = False
 move_right = False
 move_up = False
 move_down = False
 
-# CREATE ELEMENTS
+
+# ELEMENTS
 icecream = Graphics('./images/ice-cream-cart.png')
-icecreamX = 185
+icecreamX = 785
 icecreamY = 100
 
 dog = Graphics('./images/dog.png')
-dogX = 360
+dogX = 560
 dogY = 280
 
 danger = Graphics('./images/danger.png')
 dangerX = 185
 dangerY = 400
+danger2 = Graphics('./images/danger.png')
 
-# CREATE GOAL
-size = width, height = 900, 600 # window details
-goalX = width-266
-goalY = height-66
+size = width, height = 1300, 500 # window size
+goalX = width-270
+goalY = height-68
 goal = Graphics('./images/goal.png')
 
 # initialize game
 pygame.init()
+
+# initialize window
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Reach the hospital')
+# bg = pygame.image.load("./images/grid-bg.png")
 clock = pygame.time.Clock()
-FPS = 60
+FPS = 30
 
 # Game loop
 running = True
-start_game = False
 while running:
     screen.fill((255, 255, 255))
+#     screen.blit(bg, (0, 0))
+    player.draw()
+    addGameRect(screen, width-200, height)
+    icecream.draw(icecreamX, icecreamY)
+    dog.draw(dogX, dogY)
+    goal.draw(goalX, goalY)
+    danger.draw(dangerX, dangerY) 
+    danger.drawRect()   
+    danger2.draw(1110, 85, scale = (50, 50)) # draw danger icon outside game window
 
-    if not start_game: # game has not started and player is in main menu
-        start_font = pygame.font.Font('freesansbold.ttf', 28)
-        start_text = start_font.render('Click Start to begin', True, (0, 0, 0))
-        screen.blit(start_text, (230, 100))
-        if start_button.draw(screen):
-            start_game = True
-        if exit_button.draw(screen):
-            running = False
     
-    else:
-        player.draw()
-        addGameRect(screen, width-200, height)
-        icecream.draw(icecreamX, icecreamY)
-        dog.draw(dogX, dogY)
-        goal.draw(goalX, goalY)
-        danger.draw(dangerX, dangerY)
-
-        # add risk cost reward details
-        danger.draw(750, 10) # draw danger icon outside game window
-        risk.write(loc = (750, 70), color = (255, 0, 0))
-        cost.write(loc = (750, 85), color = (255, 0, 0))
-        reward.write(loc = (750, 100), color = (255, 0, 0))
-
-        player.move(move_left, move_right, move_up, move_down)
+    # DISPLAY TEXT OUTSIDE GAME WINDOW
+    legend = Text('LEGEND')
+    legend.write(loc = (1135, 20), color = (0, 0, 0), font_size=24)
+    for i in range(len(probabilities)):
+        prob = probabilities[i]
+        prob.write(loc = (1120, 170+i*20), color = (0, 0, 200))
+    
+    for j in range(len(rrc)):
+        value = rrc[j]
+        value.write(loc = (1170, 80+j*20), color = (0, 0, 200))
+    
+    # MOVE PLAYER
+    player.move(move_left, move_right, move_up, move_down)
 
 
     for event in pygame.event.get():
@@ -217,22 +207,54 @@ while running:
             if event.key == pygame.K_DOWN:
                 move_down = False                
 
-    if reachedGoal(player.rect.x, player.rect.y):
+    # CHECK FOR COLLISIONS
+    if isCollide(player, goal):
+        player.collide(goal)
         over_font = pygame.font.Font('freesansbold.ttf', 28)
         game_over_text = over_font.render('Game over!', True, (0, 0, 0))
         screen.blit(game_over_text, (300, 150))
+
+    if isCollide(player, dog):
+        player.collide(dog)
         
-    if reachedDanger(player.rect.x, player.rect.y):
+    if isCollide(player, icecream):
+        player.collide(icecream)
+        
+    if isCollide(player, danger):
+        player.collide(danger)
+        R = prob_risk_avoidance * risk
+        P = prob_profit_seeking * (reward-cost)
+        net = P-R # USE ABSOLUTE VALUE HERE?
+        rand = np.random.random()
+        # rand = r1.value
+        print(rand, net)
         if rand < net:
-            print('Stealing')
-            string = format(net, '.2f') + ' ' + format(rand, '.2f') + ' ' + str(player.steal)
+            print('R: %f\nP: %f\nNet: %f\nrand: %f' % (R, P, net, rand))
+            print('Player has decided to steal')
+            string = 'Net: %.2f, rand: %.2f, STEALING' % (net, rand)
             player.steal = 1
+            rand2 = np.random.random()
+            # rand2 = r2.value
+            if rand2 < risk:
+                prob_risk_avoidance -= learning_rate
+                string = 'CAUGHT. Risk avoidance: %.2f, rand2: %.2f' % (prob_risk_avoidance, rand2)
+                print('Player got caught')
+                running = False
+
+            else:
+                print('Did not get caught')
+                running = False
+
+            
         else:
-            string = 'Skip'
+            string = 'Not stealing'
+            running = False
+        
             
         font = pygame.font.Font('freesansbold.ttf', 24)
-        text = font.render(string, True, (0, 0, 0))
-        screen.blit(text, (300, 150))        
+        text = font.render(string, True, (0, 0, 180))
+        screen.blit(text, (300, 150))
+
 
     clock.tick(FPS)
     pygame.display.update()
